87a9cf90cd868a9840fd8f4c337424e153fdd9f3
StefanSchmelz <sschmelz64@gmail.com>
2019-05-23 11:20:28 +0200

    🏗 Added setup.py
    Added a setup.py to potentially make this a python package in the Future.

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md") as fd:
+    long_description = fd.read()
+
+setuptools.setup(
+    name='Overlayer',
+    version='0.1',
+    srcipts=[],
+    author='Neo Cortex',
+    author_email='',
+    description='Creates Images for Stream overlays that look like terminal ui',
+    long_description=long_description,
+    url='https://github.com/StefanSchmelz/StreamOverlay',
+    packages=setuptools.find_packages(),
+    classifiers=[
+      "Programming Language :: Python :: 3",
+      "License :: MIT License",
+      "Operating System :: OS Independent"
+    ]
+)
